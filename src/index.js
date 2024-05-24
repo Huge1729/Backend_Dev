5ab0de4164675124477f87ff948f722c610d4a24
Huge1729 <vishthehuge22@gmail.com>
2024-05-24 11:54:06 +0530

    Connected to MongoDB

---
+++ src/index.js
@@ -0,0 +1,40 @@
+// require('dotenv').config({ path: './env' })
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+dotenv.config({
+    path: './.env'
+})
+// console.log(process.env.MONGODB_URI)
+connectDB()
+
+
+
+
+
+
+/*
+import { DB_NAME } from "./constants"
+import mongoose from "mongoose"
+
+import express from "express"
+const app = express() 
+//  iffi fn
+( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("errror", (error) => {
+            console.log("ERRR: ", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+
+    } catch (error) {
+        console.error("ERROR: ", error)
+        throw err
+    }
+})()
+
+*/
